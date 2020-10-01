e10e7bbf03ba44b9fec154c2b6a41c683ef0e49d
sunny <sunny_ye@outlook.com>
2020-10-01 18:30:29 +0800

    update recommendation

--- functions/index.js
+++ functions/index.js
@@ -8,61 +8,77 @@
 //   response.send("Hello from Firebase!");
 // });
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors({ origin: true }));
 // var admin = require("firebase-admin");
-
 
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://dip-gr8.firebaseio.com"
+  databaseURL: "https://dip-gr8.firebaseio.com",
 });
 
 const db = admin.firestore();
 
+app.get("/cat/:keyword", (req, res) => {
+  (async () => {
+    try {
+      var keyword = req.params.keyword.toLowerCase();
+      if (keyword.includes("health")){
+        keyword = "health";
+      } else if (keyword.includes("knowledge") || keyword.includes("study")){
+        keyword = "study";
+      } else if (keyword.includes("personal")){
+        keyword = "personal";
+      }
+      let query = db
+        .collection("recommendation")
+        .where("cat", "==", keyword);
+      let response = [];
+      const snapshoot = await query.get();
+      if (snapshoot.empty) {
+        query = db.collection("recommendation").where("cat", "==", "random");
+      }
 
-app.get('/cat/:keyword', (req, res) => {
-    (async () => {
-        try {
-            let query = db.collection('recommendation').where('cat', 'in', [req.params.keyword, "random"]);
-            let response = [];
-            await query.get().then(querySnapshot => {
-            let docs = querySnapshot.docs;
-            for (let doc of docs) {
-                const selectedItem = {
-                    cat: doc.data().cat,
-                    content: doc.data().content
-                };
-                response.push(selectedItem);
-            }
-            return null;
-            });
-            return res.status(200).send(response);
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
+      await query.get().then((querySnapshot) => {
+        let docs = querySnapshot.docs;
+        for (let doc of docs) {
+          const selectedItem = {
+            cat: doc.data().cat,
+            content: doc.data().content,
+          };
+          response.push(selectedItem);
         }
-        })();
+        return null;
+      });
+
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
 // create
-app.post('/api/create', (req, res) => {
-    (async () => {
-        try {
-          await db.collection('recommendation').doc('/' + req.body.id + '/')
-              .create({cat: req.body.cat,content:req.body.content});
-          return res.status(200).send();
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send(error);
-        }
-      })();
-  });
+app.post("/api/create", (req, res) => {
+  (async () => {
+    try {
+      await db
+        .collection("recommendation")
+        .doc("/" + req.body.id + "/")
+        .create({ cat: req.body.cat, content: req.body.content });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
 
 exports.app = functions.https.onRequest(app);
